0bc61696d4e55738b847c86c8ba5847b6a3c97e7
Vignesh S <95733152+Vsekar05@users.noreply.github.com>
2022-03-25 10:39:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,106 @@
-import os
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
-import plotly.express as px
 import pandas as pd
+import numpy as np
+import streamlit as st
+from scipy.sparse import csr_matrix
+from sklearn.neighbors import NearestNeighbors
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import confusion_matrix
+import re
+import string
 
 
-app = dash.Dash(__name__)
-server = app.server
+Data=pd.read_csv("https://raw.githubusercontent.com/Vsekar05/Datasets/main/netflix.csv")
+Data["user rating score"] = Data["user rating score"].dropna()
+mean_value=np.mean(Data["user rating score"])
+Data["user rating score"].fillna(value=mean_value, inplace=True)
 
-df = pd.read_csv('https://gist.githubusercontent.com/chriddyp/5d1ea79569ed194d432e56108a04d188/raw/a9f9e8076b837d541398e999dcbac2b2826a81f8/gdp-life-exp-2007.csv')
+le=preprocessing.LabelEncoder()
+Data["rating"]=le.fit_transform(Data["rating"])
 
-fig = px.scatter(df, x="gdp per capita", y="life expectancy",
-                 size="population", color="continent", hover_name="country",
-                 log_x=True, size_max=60)
+Data["ratingLevel"].fillna(value="Parents strongly cautioned. May be unsuitable for children ages 14 and under.", inplace=True)
 
-app.layout = html.Div([
-    dcc.Graph(
-        id='life-exp-vs-gdp',
-        figure=fig
-    )
-])
+def text_clean_1(text):
+    text = text.lower()
+    text = re.sub('\[.*?\]', '', text)
+    text = re.sub('[%s]' % re.escape(string.punctuation), '', text)
+    text = re.sub('\w*\d\w*', '', text)
+    text = re.sub('[‘’“”…]', '', text)
+    text = re.sub('\n', '', text)
+    return text
+
+cleaned1 = lambda x: text_clean_1(x)
+
+Data['cleaned_ratinglevel'] = pd.DataFrame(Data.ratingLevel.apply(cleaned1))
 
 
-if __name__ == '__main__':
-    app.run_server(debug=True)
+Independent_var = Data.cleaned_ratinglevel
+Dependent_var = Data.rating
+
+IV_train, IV_test, DV_train, DV_test = train_test_split(Independent_var, Dependent_var, test_size = 0.1, random_state = 225)
+
+tvec = TfidfVectorizer()
+clf2 = LogisticRegression(solver = "lbfgs")
+
+model = Pipeline([('vectorizer',tvec),('classifier',clf2)])
+
+model.fit(IV_train, DV_train)
+
+predictions = model.predict(IV_test)
+
+confusion_matrix(predictions, DV_test)
+
+Show_Id=[]
+for x in range(1,1001):
+  Show_Id.append(x)
+Data["Show_Id"]=Show_Id
+
+New_Data=Data.pivot(index="rating",columns="Show_Id",values="user rating score")
+
+New_Data.fillna(0,inplace=True)
+
+csr_data = csr_matrix(New_Data.values)
+New_Data.reset_index(inplace=True)
+
+knn = NearestNeighbors(metric='cosine', algorithm='brute', n_neighbors=20, n_jobs=-1)
+knn.fit(csr_data)
+
+def get_show_recommendation(show_name):
+    n_movies_to_reccomend = 10
+    movie_list = Data[Data['title'].str.contains(show_name)]  
+    if len(movie_list):        
+        movie_idx= movie_list.iloc[0]['rating']
+        movie_idx = New_Data[New_Data['rating'] == movie_idx].index[0]
+        
+        distances , indices = knn.kneighbors(csr_data[movie_idx],n_neighbors=n_movies_to_reccomend+1)    
+        rec_movie_indices = sorted(list(zip(indices.squeeze().tolist(),distances.squeeze().tolist())),\
+                               key=lambda x: x[1])[:0:-1]
+        
+        recommend_frame = []
+        
+        for val in rec_movie_indices:
+            movie_idx = New_Data.iloc[val[0]]['rating']
+            idx = Data[Data['rating'] == movie_idx].index
+            recommend_frame.append({'Title':Data.iloc[idx]['title'].values[0],'Distance':val[1]})
+        df = pd.DataFrame(recommend_frame,index=range(1,n_movies_to_reccomend+1))
+        return df
+    
+    else:
+        
+        return "No movies found. Please check your input"
+
+st.set_page_config(
+    page_title="Netflix shows rating analyser and Recomendation",
+    page_icon="https://miro.medium.com/max/807/1*uxkCLCVoxLJlkoPTFDoMLg.jpeg",
+    layout="centered", initial_sidebar_state="auto"
+)
+rating_level=st.text_input(label="Give the rating level",value="",max_chars=200, key=None, type="default",
+              placeholder=None, disabled=False)
+st.write('The rating for the given movie is',model.predict(rating_level))
+Show_name=st.text_input(label="Give the show title",value="",max_chars=200, key=None, type="default",
+              placeholder=None, disabled=False)
+ 
+st.write("The shows which are similar in rating as the show which you have given are",get_show_recommendation(Show_name))
